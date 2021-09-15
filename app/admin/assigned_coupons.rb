fabe1da1ef3148459d5b55ebd85e702fee0bca10
anonymous anonymous <wsun3@uchicago.edu>
2021-09-15 20:07:34 +0000

    Whitelist columns in AssignedCoupon for ActiveAdmin

--- app/admin/assigned_coupons.rb
+++ app/admin/assigned_coupons.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register AssignedCoupon do
 
+ permit_params :coupons_id, :customers_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
