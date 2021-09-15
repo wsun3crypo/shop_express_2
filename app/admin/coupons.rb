7a5c72cc9ee8136c3ac95f2b5c5318fecbb6c8a4
anonymous anonymous <wsun3@uchicago.edu>
2021-09-15 20:07:23 +0000

    Whitelist columns in Coupon for ActiveAdmin

--- app/admin/coupons.rb
+++ app/admin/coupons.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Coupon do
 
+ permit_params :merchants_id, :amount, :expiry_date
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
