fc95a9c88bcd58f61524b477d6b41fd9c7a0be98
anonymous anonymous <wsun3@uchicago.edu>
2021-09-15 20:07:33 +0000

    Whitelist columns in CustomerFriend for ActiveAdmin

--- app/admin/customer_friends.rb
+++ app/admin/customer_friends.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CustomerFriend do
 
+ permit_params :sender_id, :receiver_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
