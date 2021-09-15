280db1a7a93767b3501c90ee96142c8c4e6add9b
anonymous anonymous <wsun3@uchicago.edu>
2021-09-15 20:07:36 +0000

    Whitelist columns in ProductReview for ActiveAdmin

--- app/admin/product_reviews.rb
+++ app/admin/product_reviews.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ProductReview do
 
+ permit_params :review, :customers_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
